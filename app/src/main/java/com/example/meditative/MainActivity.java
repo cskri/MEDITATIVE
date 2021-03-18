502ee1ad31994e6e78d577cf49c4ca4ca42a00b7
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2021-03-18 11:18:05 +0100

    Splash

--- app/src/main/java/com/example/meditative/MainActivity.java
+++ app/src/main/java/com/example/meditative/MainActivity.java
@@ -3,20 +3,13 @@ package com.example.meditative;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
 import android.view.WindowManager;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 3000;
+    private static final int SPLASH_SCREEN = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +17,15 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_SCREEN);
+        new Handler().postDelayed(this::run,SPLASH_SCREEN);
 
 
 
     }
 
+    private void run () {
+        Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(homeIntent);
+        finish();
+    }
 }
