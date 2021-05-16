5620c3de4111b0ee2b2cc8591f8e7720ece6bb3c
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2021-05-16 15:24:15 +0200

    Music Implementation

--- app/src/main/java/com/example/meditative/LoginActivity.java
+++ app/src/main/java/com/example/meditative/LoginActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private static LoginActivity instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
